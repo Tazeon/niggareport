6963aa1fe45e6d7ea27d9152b7f950cd4acc29df
Tazeon <nfocus.2549@gmail.com>
2025-04-02 22:21:01 +0700

    End JS Module

--- New Arrival/module.js
+++ New Arrival/module.js
@@ -5,3 +5,12 @@ export function add(a,b){
 export function greet(){
     return "Hello World";
 }
+
+export const name="Jane Doe";
+
+export const person ={
+    name : "Jane Doe",
+    age : 25
+}
+
+export const td = [10,20,30,40,50];
--- New Arrival/script.js
+++ New Arrival/script.js
@@ -1,4 +1,7 @@
-import {add ,greet} from './module.js';
+import {add ,greet, person, td} from './module.js';
 
 console.log(add(5,20));
 console.log(greet());
+console.log(name);
+console.log(person);
+console.log(td);
