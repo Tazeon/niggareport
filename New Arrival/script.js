625dae08448dd47ed85816933e4c19800c1cec5b
Tazeon <nfocus.2549@gmail.com>
2025-04-03 23:42:04 +0700

    End fetch API JS

--- New Arrival/script.js
+++ New Arrival/script.js
@@ -6,9 +6,38 @@
 // console.log(person);
 // console.log(td);
 
-fetch("https://jsonplaceholder.typicode.com/users")
+function fetchAndDisplayUsers(){
+    fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => {
         if (!response.ok){
-            throw new Error("Network response was not ok")
+            throw new Error("Network response was not ok" + response.statusText);
         }
+        return response.json(); 
+    })
+    .then(data => {
+        console.log(data);
+        const userContainer = document.getElementById("user-container");
+        userContainer.innerHTML = ""; //All Clear existing content
+
+        data.forEach(user => {
+            const userDiv = document.createElement("div");
+            userDiv.innerHTML = `
+                <h2>${user.name}</h2>
+                <p><strong>Email : </strong>${user.email}</p>
+                <p><strong>Phone : </strong>${user.phone}</p>
+                <p><strong>Web : </strong>${user.website}</p>
+            `;
+
+            userContainer.appendChild(userDiv);
+        })
+    })
+    .catch(error => {
+        console.error("There was a problem with fetch operation",error);
     });
+}
+
+fetchAndDisplayUsers();
+
+
+
+
